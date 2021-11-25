434498d047eb7fb0a20f9d45eb84899c16d1a540
DabooUA <sbardysh@tutanota.com>
2021-11-25 11:08:03 -0500

    commit tree 553e40b56ff73a2614a92f2292f14206cc798583

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(sArray, callback){
+  const r = [];
+    for (let i = 0; i < sArray.length; i++){
+      const newSrc = sArray[i];
+      r.push(callback(newSrc));
+    }
+    return r;
+}
+
+function reduce(sArray, callback, startingPoint){
+  let total;
+  if (startingPoint){
+    total = startingPoint;
+    for (let i = 0; i < sArray.length; i++){
+      total = callback(sArray[i], total);
+    }
+    return total;
+  } else {
+    total = sArray[0];
+    for (let i = 1; i < sArray.length; i++){
+      total = callback(sArray[i], total);
+    }
+    return total;
+  }
+}
